c9e2612e87fd02e63f28cd5fee5d4d5866611659
Beymar Medrano Ticona <128946803+eduardo9496@users.noreply.github.com>
2023-09-16 21:56:20 -0300

    Delete tp4 directory

--- tp4/Comandos_y_Creditos.js
+++
@@ -1,9 +0,0 @@
-function Comandos( PantComandos , BotonAtras){
-  image(PantComandos,0,0,800,600);
-  image(BotonAtras,10,10,50,50);
-}
-
-function Creditos( PantCreditos , BotonAtras){
-  image(PantCreditos,0,0,800,600); 
-  image(BotonAtras,10,10,50,50);
-}
